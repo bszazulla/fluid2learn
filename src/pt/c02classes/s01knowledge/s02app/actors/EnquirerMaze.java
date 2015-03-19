d3ced5a19d9d911c6a49b556052f8c1a496c9d09
Gabriel Gimenez <gabrieljgimenez@gmail.com>
2015-03-19 11:26:03 -0300

    Correcao dos 2 whiles e inicio da implementacao dos modelos

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -6,84 +6,72 @@ import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 import pt.c02classes.s01knowledge.s01base.impl.DirecaoMaze;
 
-public class EnquirerMaze implements IEnquirer 
-{
+public class EnquirerMaze implements IEnquirer {
 
 	IResponder responder;
-	
-	// conexão com o responder
-	public void connect(IResponder responder) 
-	{
+
+	// conexï¿½o com o responder
+	public void connect(IResponder responder) {
 		this.responder = responder;
 	}
-	
-	public boolean discover() 
-	{
+
+	public boolean discover() {
 		// inicia com pergunta
-		String acao = "P";
 
 		String dirUltimo;
-		
-		// pilha 
+
+		// pilha
 		Stack<DirecaoMaze> pilha = new Stack<DirecaoMaze>();
-		
-		while (!acao.equalsIgnoreCase("F")) 
-		{
-			// objeto que guarda as informacoes do estado atual
-			DirecaoMaze atual = new DirecaoMaze("null");
-			
-			// ateh encontrar a saida
-			String resposta;
-			
-			do
-			{
-				resposta = responder.ask(atual.getDirecao()); // pergunta ao responder
-				
-				// passagem ou saida
-				if (resposta.equalsIgnoreCase("passagem")|| resposta.equalsIgnoreCase("saida")) 
-				{
-					if(atual.voltando()) 
-					{
-						responder.move(atual.getVeioDe()); // volta
-						System.out.println("Movimento para " + atual.getVeioDe());
-						
-						atual = pilha.pop();
-						atual.mudaDirecao();
-					}
-					else 
-					{
-						responder.move(atual.getDirecao()); // responder move
-						System.out.println("Movimento para " + atual.getDirecao());
-						
-						pilha.push(atual); // coloca na pilha
-						
-						if (!pilha.empty())
-							dirUltimo = pilha.peek().getDirecao(); // ultima direção escolhida
-						else
-							dirUltimo = "null"; // estah na entrada do labirinto e pilha vazia
-						
-						atual = new DirecaoMaze(dirUltimo); // novo objeto apontado por atual = nova tentativa de movimento
-					}
-				} 
-				// parede
-				else
-					atual.mudaDirecao(); // muda a tentativa de movimento
-				
-			} while (!resposta.equalsIgnoreCase("saida"));
-			
-			// pergunta se estah na saida e pode terminar o programa
-			if (responder.ask("aqui") == "saida") 
-			{
-				acao = "F";
+		// objeto que guarda as informacoes do estado atual
+		DirecaoMaze atual = new DirecaoMaze("null");
+
+		// ateh encontrar a saida
+		String resposta;
+
+		do {
+			resposta = responder.ask(atual.getDirecao()); // pergunta ao
+															// responder
+
+			// passagem ou saida
+			if (resposta.equalsIgnoreCase("passagem")
+					|| resposta.equalsIgnoreCase("saida")) {
+				if (atual.voltando()) {
+					responder.move(atual.getVeioDe()); // volta
+					System.out.println("Movimento para " + atual.getVeioDe());
+
+					atual = pilha.pop();
+					atual.mudaDirecao();
+				} else {
+					responder.move(atual.getDirecao()); // responder move
+					System.out.println("Movimento para " + atual.getDirecao());
+
+					pilha.push(atual); // coloca na pilha
+
+					if (!pilha.empty())
+						dirUltimo = pilha.peek().getDirecao(); // ultima direï¿½ï¿½o
+																// escolhida
+					else
+						dirUltimo = "null"; // estah na entrada do labirinto e
+											// pilha vazia
+
+					atual = new DirecaoMaze(dirUltimo); // novo objeto apontado
+														// por atual = nova
+														// tentativa de
+														// movimento
+				}
 			}
-		}
-		
+			// parede
+			else
+				atual.mudaDirecao(); // muda a tentativa de movimento
+
+		} while (!resposta.equalsIgnoreCase("saida"));
+
 		if (responder.finalAnswer("cheguei"))
-			System.out.println("Você encontrou a saida!");
+			System.out.println("Vocï¿½ encontrou a saida!");
 		else
-			System.out.println("Fuém fuém fuém!");
-		
+			System.out.println("Fuï¿½m fuï¿½m fuï¿½m!");
+
 		return true;
 	}
-	
+
 }
